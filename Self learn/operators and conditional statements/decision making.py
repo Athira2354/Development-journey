8ec0670d2225c05ca0ffa2b290befc5cdf41adb0
Athira2354 <athirapradeeep.cp@gmail.com>
2026-03-18 22:51:25 +0530

    decision making  solved programs

---
+++ Self learn/operators and conditional statements/decision making.py
@@ -0,0 +1,64 @@
+# Write a program to check whether a number is positive or negative.
+num=int(input("enter a number:"))
+if(num<0):
+    print("negative")
+else:
+    print("positive")
+
+# Write a program to check whether a number is even or odd.
+num1=int(input("enter a number :"))
+if(num1%2==0):
+    print("even")
+else:
+    print("odd")
+
+# 3.Write a program to check if a person is eligible to vote (age ≥ 18).
+
+age=int(input("enter the age:"))
+
+if age<18:
+    print(f"{age} not eligible")
+else:
+    print(f"{age} eligible")
+
+# Write a program to check if a number is divisible by 7.
+
+# Write a program to find the smallest of two numbers.
+
+# 🔹 Multiple Conditions
+
+# Write a program to find the largest among three numbers.
+
+# Write a program to check whether a number is a multiple of both 3 and 5.
+
+# Write a program to assign grades based on marks:
+
+# ≥90 → A
+
+# ≥75 → B
+
+# ≥50 → C
+
+# <50 → Fail
+
+# Write a program to check whether a year is a leap year.
+
+# Write a program to classify a number as:
+
+# Positive Even
+
+# Positive Odd
+
+# Negative
+
+# 🔹 Nested Conditions
+
+# Write a program to check if a number is divisible by both 2 and 3.
+
+# Write a program to check login credentials (username & password).
+
+# Write a program to check whether a triangle is valid based on angles.
+
+# Write a program to determine the type of triangle (equilateral, isosceles, scalene).
+
+# Write a program to check whether a character is a vowel or consonant.
